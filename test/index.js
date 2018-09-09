1701afd2ffee7fe7bba7483a47fc7d7f9d991e7d
vikr01 <vik120@icloud.com>
2018-09-08 18:21:53 -0700

    updated to babel 7

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import test from 'tape';
 import { join } from 'path';
 import { readdirSync, statSync, readFileSync } from 'fs';
-import templates from 'babel-plugin-transform-es2015-template-literals';
+import templates from '@babel/plugin-transform-template-literals';
 
 import testPlugin from './testPlugin';
 
@@ -30,20 +30,20 @@ test('babel-plugin-dynamic-import-node', (t) => {
       st.end();
     });
 
-    t.test(`works with ${folderName} and the es2015 preset`, (st) => {
-      const result = testPlugin(
-        actual,
-        ['es2015'],
-        [[templates, { spec: true }]],
-      );
-      st.equal(result.trim(), expectedES2015.trim());
-      st.end();
-    });
+    // t.test(`works with ${folderName} and the es2015 preset`, (st) => {
+    //   const result = testPlugin(
+    //     actual,
+    //     ['es2015'],
+    //     [[templates, { spec: true }]],
+    //   );
+    //   st.equal(result.trim(), expectedES2015.trim());
+    //   st.end();
+    // });
 
     t.test(`works with ${folderName} and the env preset`, (st) => {
       const result = testPlugin(
         actual,
-        ['env'],
+        ['@babel/preset-env'],
         [[templates, { spec: true }]],
       );
       st.equal(result.trim(), expectedES2015.trim());

a4bc099d437846f576eef6455737d61078003f26
vikr01 <vik120@icloud.com>
2018-09-08 19:01:30 -0700

    fixed all expected.js tests

--- test/fixtures/non-string-argument/expected.js
+++ test/fixtures/non-string-argument/expected.js
@@ -1,6 +1,8 @@
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-Promise.resolve().then(() => _interopRequireWildcard(require(`${{ 'answer': 42 }}`)));
+Promise.resolve().then(() => _interopRequireWildcard(require(`${{
+  'answer': 42
+}}`)));
 Promise.resolve().then(() => _interopRequireWildcard(require(`${['foo', 'bar']}`)));
 Promise.resolve().then(() => _interopRequireWildcard(require(`${42}`)));
 Promise.resolve().then(() => _interopRequireWildcard(require(`${void 0}`)));
